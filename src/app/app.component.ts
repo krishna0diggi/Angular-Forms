73d8e7afbb304f903bbfa32ed562176a0a6cfcf6
Krishna Kumar Saw <krishna.kumarsaw@diggibyte.com>
2024-09-30 11:13:14 +0530

    reactive-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Corrected to styleUrls
 })
-export class AppComponent {
-  title = 'forms';
+export class AppComponent implements OnInit {
+  userForms: FormGroup;
+  submittedData: any[] = [];
+
+  ngOnInit(): void {
+    this.userForms = new FormGroup({
+      id: new FormControl(null),
+      name: new FormControl('', [Validators.required]), // Required field
+      username: new FormControl('', [Validators.required]), // Required field
+      email: new FormControl('', [Validators.required, Validators.email]), // Required and must be a valid email
+      address: new FormGroup({
+        street: new FormControl('', [Validators.required]), // Required field
+        suite: new FormControl(''),
+        city: new FormControl('', [Validators.required]), // Required field
+        zipcode: new FormControl('', [Validators.required]), // Required field
+      })
+    });
+
+    // const storedData = localStorage.getItem('submittedData');
+    // if (storedData) {
+    //   this.submittedData = JSON.parse(storedData);
+    // }
+  }
+
+  onSubmit() {
+    if (this.userForms.valid) { // Check if the form is valid
+      this.submittedData.push(this.userForms.value);
+      console.log("All Data", this.submittedData);
+      // localStorage.setItem('submittedData', JSON.stringify(this.submittedData)); // Store in local storage
+      this.userForms.reset(); 
+    } else {
+      console.error("Form is invalid. Please fill out all required fields.");
+    }
+  }
 }
